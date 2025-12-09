3d50209e8b4deb500ff6b0cbd2aed98d6d1fb2ef
KSMMolina <kmartinez@ofima.com>
2025-12-09 14:16:57 -0500

    Delete doc-manag-web directory

--- doc-manag-web/src/componentes y archivos base/pipes/math.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform, inject } from "@angular/core";
-
-@Pipe({ name: "math", standalone: true })
-export class MathPipe implements PipeTransform {
-  transform(value: number, to: "ceil" | "floor" | "round" = "ceil"): number {
-    return Math[to](value);
-  }
-}
